357de024fd8933227e659f5daf79e238bf7280d1
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2021-12-26 04:34:56 -0300

    feat: INIT

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,7 @@
+// Testar retorno do tempo
+
+describe('Should return 2', () => {
+  it('should do something', () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import clear from 'node:console'
+import chalk from 'chalk'
+import figlet from 'figlet'
+
+// Fazer relógio
+// Subir repo
